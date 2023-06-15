bcd1805d7c44528dd2acf35817162c361bea2121
homegrownotes <homegrownotes@gmail.com>
2023-06-15 14:45:57 +0200

    Router incomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,15 @@
-import { useEffect, useState } from "react";
-import StyledDiv from "./StyledDiv";
-import Checkbox from "./components/Checkbox";
-import Counter from "./components/Counter";
-import fields from "./data/fields.json";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Budget from "./pages/Budget";
 
 function App() {
-
-  // STATES
-  const [fieldsValue, setFieldsValue] = useState(fields);
-  const [total, setTotal] = useState(0);
-  
-  // EFFECTS
-  useEffect(() => {
-    calculateTotal();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fieldsValue])
-
-  // LOGIC  
-  const handleUpdateCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const fieldsValueClone = [ ...fieldsValue ];
-    const tempField = fieldsValueClone.find(item => item.id === parseInt(e.target.id));    
-    if (tempField) tempField.checked = e.target.checked;
-    
-    setFieldsValue(fieldsValueClone);
-  }
-  
-  const handleUpdateNumbers = (id: number, value: number) => {
-    const fieldsValueClone = [ ...fieldsValue ];
-    const tempField = fieldsValueClone.find(item => item.id === id); 
-    
-    if (tempField && value === 1) tempField.value = tempField.value + value;
-    if (tempField && value === -1) {
-      if (tempField.value > 1) tempField.value = tempField.value + value;
-    }
-    
-    setFieldsValue(fieldsValueClone);
-  }
-
-  const calculateTotal = () => {
-    let newTotal = 0; 
-    let websiteTotal = 0; 
-    fieldsValue.forEach(item => {
-      if (item.type === "checkbox" && item.checked && !item.parentId) newTotal += item.value;
-      if (item.type === "number" && item.value && item.parentId === 0 && fieldsValue[0].checked) {
-        if (websiteTotal === 0) websiteTotal = 30;
-        websiteTotal = websiteTotal * item.value;
-      }
-    })   
-    setTotal(newTotal + websiteTotal);
-  }
-  
   return (
-    <>
-      { fields.map((item) => {
-        if (item.type === "checkbox") {          
-          return (        
-          <Checkbox 
-              id={item.id}
-              checked={item.checked}
-              label={item.label}
-              onChange={handleUpdateCheckbox}
-          />
-          );
-        } else if (item.type === "number") {
-          return (
-            <StyledDiv visible={fieldsValue[0].checked ? true : false}>
-              <Counter 
-                id={item.id}
-                label={item.label} 
-                value={item.value}
-                onChange={handleUpdateNumbers}
-              />
-            </StyledDiv>           
-          );
-        }        
-    }
-    )}
-    <p>Total price:</p>{total}
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/budget" element={<Budget />} />
+    </Routes>
+  );
 }
 
 export default App;
+
---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+type ReturnType<T> = [
+    T | undefined,
+    React.Dispatch<React.SetStateAction<T | undefined>>
+]
+
+const useLocalStorage = <T,>(
+    key: string,
+    defaultValue?: T
+): ReturnType<T> => {
+    const [value, setValue] = useState<T | undefined>(() => {
+        if (!defaultValue) return;
+        try {
+            const value =  localStorage.getItem(key);
+            return value ? JSON.parse(value) : defaultValue;
+        } catch (error) {
+            console.error(error);
+            return defaultValue;
+        }
+    });
+
+    useEffect(() => {
+        if (value) {
+            try {
+                localStorage.setItem(key, JSON.stringify(value));
+            } catch (error) {
+                console.error(error);
+            }
+        }    
+    }, [key, value]);
+    return [value, setValue]
+}
+
+export default useLocalStorage;
